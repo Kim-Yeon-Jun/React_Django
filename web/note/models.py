2b06de60f1790efbf00a4b15b1d74e399736d9f0
Kim-Yeon-Jun <106429088+Kim-Yeon-Jun@users.noreply.github.com>
2024-01-23 14:56:18 +0900

    corsheaders

--- web/note/models.py
+++ web/note/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class note(models.Model):
+    user_name = models.CharField(max_length = 20)
+    title = models.CharField(max_length = 50)
+    category = models.CharField(max_length = 20)
+    contents = models.TextField()
--- web/note/views.py
+++ web/note/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import models
 # Create your views here.
 
 def main_view(request):
     
     return HttpResponse("메인 화면")
+
+def show_user(request):
+    
+    return render()
